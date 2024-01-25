609d5d38d048b7548aa51e64fbc951a892ef905a
gurgenidzegiorgi <gurgenidzegiorgi69@gmail.com>
2024-01-24 21:49:48 -0500

    add User object interface

---
+++ src/User.ts
@@ -0,0 +1,36 @@
+type User = {
+	avatar_url: string | null;
+	bio: string | null;
+	blog: string | null;
+	company: string | null;
+	created_at: string | null;
+	email: string | null;
+	events_url: string | null;
+	followers: number | null;
+	followers_url: string | null;
+	following: number | null;
+	following_url: string | null;
+	gists_url: string | null;
+	gravatar_id: string | null;
+	hireable: string | null;
+	html_url: string | null;
+	id: number | null;
+	location: string | null;
+	login: string | null;
+	name: string | null;
+	node_id: string | null;
+	organizations_url: string | null;
+	public_gists: number | null;
+	public_repos: number | null;
+	received_events_url: string | null;
+	repos_url: string | null;
+	site_admin: boolean | null;
+	starred_url: string | null;
+	subscriptions_url: string | null;
+	twitter_username: string | null;
+	type: string | null;
+	updated_at: string | null;
+	url: string | null;
+};
+
+export default User;
